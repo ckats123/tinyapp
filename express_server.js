3d59b5fc2df9c12713941be0aa222ac9fd5394cc
Sophia <chiaki.katsuno@live.ca>
2023-09-02 20:36:36 -0700

    created user registration form

--- express_server.js
+++ express_server.js
@@ -41,6 +41,10 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/register", (req, res) => {
+  res.render("registration");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
