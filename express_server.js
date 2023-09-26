4987fed66e8637efc4fbf1e88bf16ffb3b1271b2
Sophia <chiaki.katsuno@live.ca>
2023-09-25 18:36:52 -0700

    refactor login route

--- express_server.js
+++ express_server.js
@@ -43,10 +43,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
+  const userId = req.cookies["user_id"];
+  const user = users[userId];
+
+  const templateVars = {
+    user: user,
+  };
   if (req.cookies.user_id) {
     res.redirect("/urls");
   } else {
-    res.render("login");
+    res.render("login", templateVars);
   }
 });
 
@@ -84,10 +90,16 @@ app.post("/logout", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
+  const userId = req.cookies["user_id"];
+  const user = users[userId];
+
+  const templateVars = {
+    user: user,
+  };
   if (req.cookies.user_id) {
     res.redirect("/urls");
   } else {
-    res.render("registration");
+    res.render("registration", templateVars);
   }
 });
 
@@ -147,8 +159,15 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
+  const userId = req.cookies["user_id"];
+  const user = users[userId];
+
+  const templateVars = {
+    user: user,
+  };
+
   if (req.cookies.user_id) {
-    res.render("urls_new");
+    res.render("urls_new", templateVars);
   } else {
     res.redirect("/login");
   }
